512853b6f613b5f7decbf490aea22cd388e305f5
Ben Foppa <benjamin.foppa@gmail.com>
2013-02-19 00:04:26 -0500

    Member transformers for Note

--- src/Sound/MIDI/Monad/Types.hs
+++ src/Sound/MIDI/Monad/Types.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude
+           , TemplateHaskell
+           #-}
 module Sound.MIDI.Monad.Types ( Note (..)
                               , Tick
+                              , pitch'
+                              , instr'
+                              , vcty'
                               ) where
 
+import Prelewd
+
 import Data.Word
+import Template.MemberTransformer
+import Text.Show
 
 data Note = Note
         { pitch :: Word8    -- ^ In semitones. 48 is middle C
@@ -10,6 +20,8 @@ data Note = Note
         , vcty :: Word8
         }
     deriving (Eq, Show)
+
+$(memberTransformers ''Note)
 
 -- | 1/96th of a beat
 type Tick = Word32
