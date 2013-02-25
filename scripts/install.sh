fe61fa437f74f4ab2b1ff35f5d1cefbb10f2a885
Ben Foppa <benjamin.foppa@gmail.com>
2013-02-25 16:38:04 -0500

    script updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+commit=$1; shift 1
+
 rm -rf /tmp/testbuild &&
 mkdir -p /tmp/testbuild &&
 echo Creating temporary build environment in /tmp/testbuild &&
 cp -r --preserve=timestamps . "/tmp/testbuild" &&
 
 cd "/tmp/testbuild" &&
-git reset --hard $1 > /dev/null &&
+git reset --hard $commit > /dev/null &&
 git clean -fd > /dev/null &&
-cabal install 2>&1 | grep -v "^\(You are using a new version of LLVM that hasn't been tested yet\|We will try though\)" &&
+cabal install $@ 2>&1 | grep -v "^\(Loading package\|You are using a new version of LLVM that hasn't been tested yet\|We will try though\)" &&
 echo
