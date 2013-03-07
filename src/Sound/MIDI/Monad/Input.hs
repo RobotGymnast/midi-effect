b0c9b8b2d15219710584d7cb497ba8edd43b686e
Ben Foppa <benjamin.foppa@gmail.com>
2013-03-07 13:09:57 -0500

    Fixed releasing of MIDI inputs

--- src/Sound/MIDI/Monad/Input.hs
+++ src/Sound/MIDI/Monad/Input.hs
@@ -21,11 +21,12 @@ midiIn = ioMIDI $ \cxt -> io $ midiInIO $ seqT cxt
                 n <- E.inputPending h True
                 iff (n == 0)
                     (return [])
-                    $ fromEvent <$> E.input h <&> try (:) <*> midiInIO h
+                    (fromEvent <$> E.input h <&> try (:) <*> midiInIO h)
 
 fromEvent :: E.T -> Maybe (Bool, Note)
 fromEvent e = case E.body e of
-        E.NoteEv E.NoteOn note -> Just (True, fromALSA note)
-        E.NoteEv E.NoteOff note -> Just (False, fromALSA note)
+        E.NoteEv E.NoteOn note -> let alsa = fromALSA note
+                                  in Just (vcty alsa > 0, alsa)
+        E.NoteEv E.NoteOff _ -> error "ALSA input NoteOff event"
         E.CtrlEv _ _ -> Nothing
         _ -> traceShow e undefined
