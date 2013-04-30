0de3a9271b3c2e0afc2644f621db7056b615e00b
Ben Foppa <benjamin.foppa@gmail.com>
2013-04-30 14:15:40 -0400

    script update

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,5 +10,7 @@ cp -r --preserve=timestamps . "/tmp/testbuild" &&
 cd "/tmp/testbuild" &&
 git reset --hard $commit > /dev/null &&
 git clean -fd > /dev/null &&
-cabal install $@ 2>&1 | grep -v "^\(Loading package\|You are using a new version of LLVM that hasn't been tested yet\|We will try though\)" &&
+cabal install $@ 2>&1 | grep -v "^\(Loading package\|You are using a new version of LLVM that hasn't been tested yet\|We will try though\)"
+ret=$PIPESTATUS
 echo
+exit $ret
